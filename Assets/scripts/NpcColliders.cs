cf6bec6c74b5254272ccf3d4b2b6c97c54b1dd22
int04 <trandoducnghia@gmail.com>
2024-09-24 22:18:44 +0700

    [Update] danh sách NPC.

---
+++ Assets/scripts/NpcColliders.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class NpcColliders : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI textName;
+    [SerializeField] private string setTextName = "";
+
+    private void Start()
+    {
+        if (setTextName.Length >= 1)
+        {
+            textName.text = setTextName;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        print("va chạm với" + other.gameObject.name);
+    }
+
+}
--- Assets/scripts/PlayerControllers.cs
+++ Assets/scripts/PlayerControllers.cs
@@ -80,4 +80,6 @@ public class PlayerControllers : MonoBehaviour
 
 
     }
+
+
 }
