db469f0f5a8bbe069f605247fed613d89144b735
int04 <trandoducnghia@gmail.com>
2024-09-25 06:33:49 +0700

    [Update] cập nhaajt các nhân vật, cập nhật di chuyển map ở menu chào mừng.

--- Assets/scripts/PlayerControllers.cs
+++ Assets/scripts/PlayerControllers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerControllers : MonoBehaviour
@@ -10,6 +11,26 @@ public class PlayerControllers : MonoBehaviour
     [SerializeField] private Animator animator;
 
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private TextMeshProUGUI namePlayer;
+    [SerializeField] private string setNamePlayer = "";
+    [SerializeField] private Canvas canvas;
+    private void Awake()
+    {
+        if (setNamePlayer.Length >= 1)
+        {
+            namePlayer.text = setNamePlayer;
+        }
+    }
+
+    public void SetNamePlayer(string namec)
+    {
+        namePlayer.text = namec;
+    }
+    public void SetDefaultXY()
+    {
+        gameObject.transform.position = new Vector3(-13, 6, 0);
+    }
+
     Dictionary<string, bool> _keys = new Dictionary<string, bool>()
     {
         {"up", false},
@@ -31,6 +52,17 @@ public class PlayerControllers : MonoBehaviour
             k.x *= -1;
         }
         gameObject.transform.localScale = k;
+
+        var m = canvas.transform.localScale;
+        if (right && m.x < 0)
+        {
+            m.x *= -1;
+        }
+        else if (right == false && m.x > 0)
+        {
+            m.x *= -1;
+        }
+        canvas.transform.localScale = m;
     }
 
 
--- Assets/scripts/StartGameControllers.cs
+++ Assets/scripts/StartGameControllers.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using Cinemachine;
+using TMPro;
 using UnityEngine;
 /*
  * @since04
@@ -12,6 +14,61 @@ public class StartGameControllers : MonoBehaviour
     private int _index = 0;
     private List<GameObject> _listNPC = new List<GameObject>();
     [SerializeField] private GameObject transformNPC;
+
+    [SerializeField] private List<PlayerControllers> playerControllers = new List<PlayerControllers>();
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+    [SerializeField] private TMP_InputField inputName;
+    private bool _isStart = false;
+    [SerializeField] private GameObject noLogin;
+
+
+
+    public void MenuChinh()
+    {
+        cinemachineVirtualCamera.Follow = noLogin.transform;
+    }
+
+    private void Start()
+    {
+        ResetNPC();
+        MenuChinh();
+    }
+
+    private float _xMin = -6;
+    private float _yMin = -1.18f;
+    private float _yMax = 14f;
+    private float _xMax = 95f;
+    private float Rand(float min, float max)
+    {
+        return UnityEngine.Random.Range(min, max);
+    }
+
+    private bool _moveRight = true;
+    private void Update()
+    {
+        if (_isStart) return;
+
+
+        // Điểm đích cuối cùng
+        Vector3 targetPosition = new Vector3(
+            _moveRight ? _xMax : _xMin,
+            noLogin.transform.position.y,
+            0);
+        // Sử dụng Lerp để di chuyển mượt mà hơn
+        noLogin.transform.position = Vector3.Lerp(
+            noLogin.transform.position,
+            targetPosition,
+            0.01f * Time.deltaTime); // Điều chỉnh tốc độ với deltaTime
+
+        if (noLogin.transform.position.x >= _xMax)
+        {
+            _moveRight = false;
+        }
+        else if (noLogin.transform.position.x <= _xMin)
+        {
+            _moveRight = true;
+        }
+    }
 
     private void Awake()
     {
@@ -26,6 +83,12 @@ public class StartGameControllers : MonoBehaviour
         foreach (var t in _listNPC)
         {
             t.SetActive(true);
+        }
+
+        foreach (var k in playerControllers)
+        {
+            k.SetDefaultXY();
+            k.gameObject.SetActive(false);
         }
     }
 
@@ -43,7 +106,15 @@ public class StartGameControllers : MonoBehaviour
 
     public void InterGame()
     {
+        _isStart = true;
         ResetNPC();
+        playerControllers[_index].gameObject.SetActive(true);
+        cinemachineVirtualCamera.Follow = playerControllers[_index].transform;
+        gameObject.SetActive(false);
+        if(inputName.text.Length >= 1)
+        {
+            playerControllers[_index].SetNamePlayer(inputName.text);
+        }
     }
 
     public void SelectChar()
